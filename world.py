0e1e31f0b15fefa187dd086544d15c423707274b
Nkosiyapha <realnkosi@gmail.com>
2024-05-31 17:56:32 +0200

    Added some code

--- world.py
+++ world.py
@@ -1,6 +1,10 @@
 def myfunc(name):
     for i in range(8):
-        print(i)
+        print(name)
+
+def hhy():
+    print("dfsdfs")
 
 myfunc("This is Nkosi's Git commit.") 
 myfunc("This is realnkosi's Got commit")
+
